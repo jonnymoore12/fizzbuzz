bb4e96c1768088b129d909de8713aa3917bde88b
benjamin-white <benjaminwhite@live.co.uk>
2016-06-30 22:24:38 +0100

    added extra tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,4 +4,10 @@ describe 'fizzbuzz' do
    it 'returns "fizz" when passed 3' do
       expect(fizzbuzz(3)).to eq 'fizz'
    end
+   it 'returns "buzz" when passed 5' do
+      expect(fizzbuzz(5)).to eq 'buzz'
+   end
+   it 'returns "12" when passed 12' do
+      expect(fizzbuzz(12)).to eq 12
+   end
 end
