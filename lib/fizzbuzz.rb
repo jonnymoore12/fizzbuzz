395619dafe62ed892dd5d45d1545a573689279af
Jonny Moore <jonnymoore121212@gmail.com>
2016-06-30 22:55:03 +0100

    Added test for 0

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(number)
-   if number == 3
+   if number % 3 == 0 && number % 5 == 0
+      'fizzbuzz'
+   elsif number % 3 == 0
       'fizz'
-   elsif number == 5
+   elsif number % 5
       'buzz'
-   elsif number % 3 == 0 && number % 5 == 0
-       'fizzbuzz'
    else
       number
    end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -10,19 +10,22 @@ describe 'fizzbuzz' do
    it 'returns "fizz" when passed 3' do
       expect(fizzbuzz(6)).to eq 'fizz'
    end
-   it 'returns "buzz" when passed 5' do
+   it 'returns "buzz" when passed 25' do
       expect(fizzbuzz(25)).to eq 'buzz'
    end
    it 'returns "fizzbuzz" when passed a multiple of 3 and 5' do
       expect(fizzbuzz(15)).to eq 'fizzbuzz'
    end
    it 'returns "fizzbuzz" when passed a multiple of 3 and 5' do
-      expect(fizzbuzz(35)).to eq 'fizzbuzz'
+      expect(fizzbuzz(35)).to eq 'buzz'
    end
    it 'returns "12" when passed 12' do
-      expect(fizzbuzz(12)).to eq 12
+      expect(fizzbuzz(12)).to eq 'fizz'
    end
    it 'returns "24" when passed 24' do
-      expect(fizzbuzz(24)).to eq 24
+      expect(fizzbuzz(24)).to eq 'fizz'
+   end
+   it 'returns "0" when passed 0' do
+      expect(fizzbuzz(0)).to eq 0
    end
 end
